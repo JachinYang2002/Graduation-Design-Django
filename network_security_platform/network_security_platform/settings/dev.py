65c355b621a5fa89e1f4cfb2b4bddbd89bcec2a2
JachinYang <1799487754@qq.com>
2024-08-25 11:50:17 +0800

    配置mysql数据库

--- network_security_platform/network_security_platform/settings/dev.py
+++ network_security_platform/network_security_platform/settings/dev.py
@@ -90,8 +90,12 @@ WSGI_APPLICATION = 'network_security_platform.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'network_security_platform_db',
+        'USER': 'root',
+        'PASSWORD': 'rrxx220605.',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
